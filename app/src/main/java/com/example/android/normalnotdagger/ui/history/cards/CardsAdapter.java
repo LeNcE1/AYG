2d3a265c0a73ac068bb3a4fe0eaa9108f0020993
Алексей Бузин <lence.gamer@gmail.com>
2017-10-11 16:07:53 +0800

    галерея

--- app/src/main/java/com/example/android/normalnotdagger/ui/history/cards/CardsAdapter.java
+++ app/src/main/java/com/example/android/normalnotdagger/ui/history/cards/CardsAdapter.java
@@ -1,17 +1,23 @@
 package com.example.android.normalnotdagger.ui.history.cards;
 
 
+import android.content.Context;
 import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.android.normalnotdagger.R;
 import com.example.android.normalnotdagger.models.new_model.categ_model.Card;
+import com.squareup.picasso.Picasso;
+import com.veinhorn.scrollgalleryview.MediaInfo;
+import com.veinhorn.scrollgalleryview.ScrollGalleryView;
+import com.veinhorn.scrollgalleryview.loader.MediaLoader;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,22 +54,47 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardsViewHol
     }
 
     @Override
-    public void onBindViewHolder(CardsViewHolder holder, final int position) {
-        if(position == (pag-6)){
-            presentr.getCars(id,pag);
-            pag+=20;
+    public void onBindViewHolder(final CardsViewHolder holder, final int position) {
+        if (position == (pag - 6)) {
+            presentr.getCars(id, pag);
+            pag += 20;
         }
+        holder.images = cards.get(position).getImages();
         holder.title.setText(cards.get(position).getTitle());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            holder.text.setText(Html.fromHtml(cards.get(position).getText(),Html.FROM_HTML_MODE_LEGACY));
-        }
         holder.layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 mvp.itemClick(cards.get(position));
+                mvp.itemClick(cards.get(position));
             }
         });
-        
+
+
+        holder.infos = new ArrayList<>(holder.images.size());
+        for (final String url : holder.images)
+            holder.infos.add(MediaInfo.mediaLoader(new MediaLoader() {
+                @Override
+                public boolean isImage() {
+                    return false;
+                }
+
+                @Override
+                public void loadMedia(Context context, ImageView imageView, SuccessCallback callback) {
+                    Picasso.with(context).load(url).into(imageView);
+                    callback.onSuccess();
+                }
+
+                @Override
+                public void loadThumbnail(Context context, ImageView thumbnailView, SuccessCallback callback) {
+
+                }
+            }));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            holder.text.setText(Html.fromHtml(cards.get(position).getText(), Html.FROM_HTML_MODE_LEGACY));
+        }
+
+
+        holder.scrollGalleryView.addMedia(holder.infos);
     }
 
     @Override
@@ -80,12 +111,17 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardsViewHol
         TextView text;
         @BindView(R.id.layout)
         LinearLayout layout;
-
+        @BindView(R.id.scroll_gallery_view)
+        ScrollGalleryView scrollGalleryView;
+        List<String> images;
+        List<MediaInfo> infos;
+        View view;
 
         public CardsViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            view = itemView;
         }
     }
-    
+
 }
